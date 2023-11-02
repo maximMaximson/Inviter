0a1e211bd28c453ad014f5f96e867ffa4cbb21d8
maximMaximson <145776433+maximMaximson@users.noreply.github.com>
2023-11-02 16:33:07 +0300

    first commit

---
+++ src/components/Success.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Success = ({ count }) => {
+  return (
+    <div class="success-block">
+      <img src="/assets/success.svg" alt="Success" />
+      <h3>Успешно!</h3>
+      <p>Всем {count} пользователям отправлено приглашение.</p>
+      <a style={{ textAlign: "center" }} href="/" className="send-invite-btn">Назад </a>
+    </div>
+  );
+};
